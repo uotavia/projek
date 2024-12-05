e5927ebb3c1c41203be1e80283c9bab7965d1b2a
uotavia <uotavia@gmail.com>
2024-12-05 13:14:56 +0700

    mmm

--- PengisianKrs.java
+++ PengisianKrs.java
@@ -1,9 +1,14 @@
+import java.util.Scanner;
 public class PengisianKrs {
+    static String arrKolom[]={"Nama", "Nim", "Jenis", "Tingkat", "Tahun"};
+    static int jmlBaris = 0;
+    static String arrKrs [][] = new String[1][5];
+    static int[] arrTahun = new int[1];
     public static void main(String[] args) {
         
     }
     static void TambahKrs(){
-
+        
     }
     static void TampilKrs(){
 
