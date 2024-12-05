a07ca42a6a6d7bf3760f04a98c33e23e6d0a808b
uotavia <uotavia@gmail.com>
2024-12-05 13:26:27 +0700

    mmm

--- PengisianKrs.java
+++ PengisianKrs.java
@@ -5,10 +5,87 @@ public class PengisianKrs {
     static String arrKrs [][] = new String[1][5];
     static int[] arrTahun = new int[1];
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("=== PENCATATAN PRESTASI MAHASISWA ===");
+        System.out.println("1. Tambah data KRS \n2. Tampilkan daftar KRS mahasiswa");
+        System.out.println("3. Filter KRS \n0. Keluar");
+        System.out.print("Pilih menu: ");
+        String menu = sc.nextLine();
+
+        switch (menu) {
+            case "1":
+                TambahKrs();
+                break;
+            case "2":
+                TampilKrs();
+                break;
+            case "3":
+                FilterKrs();
+                break;
+            case "0": break;
+            default:
+                System.out.print("Menu yang anda masukkan salah \n1. Kembali\n2. Keluar\nInput pilihan: ");
+                String salah = sc.nextLine();
+                switch (salah) {
+                    case "1":
+                        System.out.println();
+                        main(args);
+                        break;
+                    case "2": break;
+                    default:
+                        System.out.println("Menu yang anda masukkan salah!");
+                        break;
+                }
+                break;
+        }
         
     }
     static void TambahKrs(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("\n-- TAMBAH DATA KRS --");
+        System.out.println("*masukkan nama 'batal123' jika ingin membatalkan aksi tambah.");
+        System.out.print("Nama mahasiswa  : ");
+        String nama = sc.nextLine();
+        if (nama.equals("batal123")) {
+            System.out.println();
+            main(null);
+        }
+        System.out.print("NIM mahasiswa   : ");
+        String nim = sc.nextLine();
+        System.out.print("Kode mata kuliah: ");
+        String jenis = sc.nextLine();
+        System.out.print("Jumlah SKS      : ");
+        int tahun = sc.nextInt();
+
         
+        //mengubah ukuran array
+        if (jmlBaris == arrKrs.length) {
+            String[][] tempArr = new String[arrKrs.length + 1][5];
+            int[] tempTahun = new int[arrTahun.length + 1]; 
+            
+            //salin data array lama
+            for (int i = 0; i < arrKrs.length; i++) {
+                tempArr[i] = arrKrs[i];
+            }
+            for (int i = 0; i < arrTahun.length; i++) {
+                tempTahun[i] = arrTahun[i];
+            }
+            //ganti array
+            arrKrs = tempArr; 
+            arrTahun = tempTahun;  
+        }
+        
+        arrKrs[jmlBaris][0] = nama;
+        arrKrs[jmlBaris][1] = nim;
+        arrKrs[jmlBaris][2] = jenis;
+        arrKrs[jmlBaris][3] = tingkat;
+        arrTahun[jmlBaris] = tahun; 
+    
+        jmlBaris++; 
+    
+        System.out.println("Data berhasil ditambahkan!");
+        main(null);
+
     }
     static void TampilKrs(){
 
